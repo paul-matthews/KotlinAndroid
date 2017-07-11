f66240691e0e814481c124ba05af9eb5b7e30f45
Paul Matthews <paul.matthews.86@gmail.com>
2017-07-11 10:12:07 -0700

    Feature: Room test - testing the Room Persistence Library (#5)
    * Adding gradle support for Room
    * Adding UI support for writing People
      * Using Recycler View for listing People
      * Inputs new users
    * Person entity
      * Basic CRUD operations on the entity
      * Thread management done through RxJava
    * Using RxJava's Flowable to responses to queries where possible
    * Adopted Maybe responses for Inserts

--- app/src/main/java/uk/co/paul_matthews/kotlinandroid/MainActivity.kt
+++ app/src/main/java/uk/co/paul_matthews/kotlinandroid/MainActivity.kt
@@ -2,77 +2,149 @@ package uk.co.paul_matthews.kotlinandroid
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import com.jakewharton.rxbinding2.view.RxView
 import io.reactivex.Observable
-import io.reactivex.ObservableOnSubscribe
-import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
-import io.reactivex.rxkotlin.subscribeBy
-import io.reactivex.schedulers.Schedulers
-import java.util.*
+import kotlinx.android.synthetic.main.activity_main.*
+import uk.co.paul_matthews.kotlinandroid.data.Person
+import uk.co.paul_matthews.kotlinandroid.data.PersonData
+
+
+val Any.tag: String
+    get() = this::class.simpleName.toString()
 
 class MainActivity : AppCompatActivity() {
-    val tag: String by lazy {
-        this::class.simpleName.toString()
+    val personData by lazy { PersonData(application) }
+    val ui by lazy {
+        Ui {
+            Log.d(tag, "Person clicked: $it")
+        }
     }
-
-    val numbersObserver: Observable<Int> =
-        Observable.create(ObservableOnSubscribe<Int> {
-            val rand = Random()
-            for (i in 1..5) {
-                if (Thread.interrupted()) {
-                    break
-                }
-                Thread.sleep(500)
-                it?.onNext(rand.nextInt(10))
-            }
-            it?.onComplete()
-        })
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .cache()
+    val subscriptions: MutableList<Disposable> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getNumbers()
-                .take(3)
-                .subscribe(getLogger("first".padEnd(7)))
-        getNumbers()
-                .subscribe(getLogger("second".padEnd(7)))
-        getNumbers()
-                .filter({ it > 5 })
-                .subscribe(getLogger("gt 5".padEnd(7)))
+        // Subscribe to all peopleData change events
+        val peopleSub = personData.getAll()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    Log.d(tag, "Found users, displaying them")
+                    ui.updateInfo(it)
+                }
+        subscriptions.add(peopleSub)
 
-        getNumbers()
-                .subscribeBy(onComplete = {
-                    getNumbers().subscribe(getLogger("delayed".padEnd(7)))
-                })
+
+        // Subscribe to all submit btn clicks
+        val submitSub = ui.getSubmitClicks().subscribe {
+            Log.d(tag, "Submit clicked.")
+            val (username, firstName, lastName) = ui.getFormData()
+            subscriptions.add(personData.insertPerson(Person(username, firstName, lastName))
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe {
+                        Log.d(tag, "Inserted user")
+                        ui.clearForm()
+                    })
+        }
+        subscriptions.add(submitSub)
+
+        // Subscribe to all reset btn clicks
+        val resetSub = ui.getResetClicks().subscribe {
+            Log.d(tag, "Reset clicked.")
+            ui.clearForm()
+        }
+        subscriptions.add(resetSub)
     }
 
-    fun getNumbers(): Observable<Int> = numbersObserver
+    override fun onDestroy() {
+        super.onDestroy()
+
+        subscriptions.map { it.dispose() }
+        subscriptions.clear()
+    }
 
     /**
-     * Get a logging subscriber
+     * Encapsulating the UI interaction
      */
-    fun getLogger(id: String): Observer<Int> = object: Observer<Int> {
-        var count = 0
-        override fun onComplete() {
-            Log.d(tag, "$id) connection complete")
+    inner class Ui(val infoItemAction: (Person) -> Unit) {
+        val info: RecyclerView = info_list
+        val usernameField: EditText = username_field
+        val firstNameField: EditText = first_name_field
+        val lastNameField: EditText = last_name_field
+        private val submitClicks = RxView.clicks(submit)
+        private val resetClicks = RxView.clicks(reset)
+        val infoLayoutManager by lazy {
+            LinearLayoutManager(this@MainActivity)
+        }
+        val infoAdapter by lazy {
+            MainInfoAdapter(mutableListOf(), infoItemAction)
+        }
+        init {
+            info.setHasFixedSize(true)
+            info.layoutManager = infoLayoutManager
+            info.adapter = infoAdapter
         }
 
-        override fun onError(e: Throwable?) {
-            Log.e(tag, "$id) Error with ${e?.message}: " + e?.stackTrace)
+        fun getSubmitClicks(): Observable<Any?> = submitClicks
+
+        fun getResetClicks(): Observable<Any?> = resetClicks
+
+        fun updateInfo(people: List<Person>) {
+            infoAdapter?.update(people)
         }
 
-        override fun onNext(t: Int?) {
-            Log.d(tag, "$id) [${++count}] Observed: $t")
-        }
+        fun getFormData(): Triple<String, String, String> = Triple(usernameField.text.toString(),
+                firstNameField.text.toString(), lastNameField.text.toString())
 
-        override fun onSubscribe(d: Disposable?) {
-            Log.d(tag, "$id) Subscribed!")
+        fun clearForm() {
+            listOf(usernameField, firstNameField, lastNameField).map {
+                it.setText("")
+            }
+        }
+    }
+}
+
+/**
+ * The Adapter for the info list view
+ */
+class MainInfoAdapter(val items: MutableList<Person>, val listener: (Person) -> Unit):
+        RecyclerView.Adapter<MainInfoAdapter.Holder>() {
+
+    override fun onBindViewHolder(holder: Holder, position: Int) =
+            holder.bind(items[position], listener)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+            Holder(inflate(parent, R.layout.person_line))
+
+    override fun getItemCount() = items.size
+
+    fun update(replacementItems: List<Person>) {
+        items.clear()
+        items.addAll(replacementItems)
+        notifyDataSetChanged()
+    }
+
+    fun inflate(parent: ViewGroup, res: Int): View =
+            LayoutInflater.from(parent.context).inflate(res, parent, false)
+
+    class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun  bind(person: Person, listener: (Person) -> Unit) = with(itemView) {
+            this.findViewById<TextView>(R.id.username).text = person.username
+            this.findViewById<TextView>(R.id.first_name).text = person.firstName
+            this.findViewById<TextView>(R.id.last_name).text = person.lastName
+            setOnClickListener {
+                listener(person)
+            }
         }
     }
 }
---
+++ app/src/main/java/uk/co/paul_matthews/kotlinandroid/data/Person.kt
@@ -0,0 +1,73 @@
+package uk.co.paul_matthews.kotlinandroid.data
+
+import android.app.Application
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
+import android.content.ContextWrapper
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import io.reactivex.Maybe
+import io.reactivex.schedulers.Schedulers
+
+/**
+ * Created by pmatthews on 6/30/17.
+ */
+
+class PersonData(val app: Application): ContextWrapper(app) {
+    fun getAll(): Flowable<List<Person>> = app.getDatabase()?.personDao()?.getAll()
+            ?.subscribeOn(Schedulers.io()) ?: Flowable.error { Error("Unable to query the database") }
+
+    fun getPeople(usernames: List<String>): Flowable<List<Person>> = app.getDatabase()?.personDao()?.getPeople(usernames)
+            ?.subscribeOn(Schedulers.io()) ?: Flowable.error { Error("Unable to query the database") }
+
+
+    fun getPerson(username: String): Maybe<Person> = Maybe.create<Person> {
+        val person = app.getDatabase()?.personDao()?.getPerson(username)
+        if (person != null) {
+            it.onSuccess(person)
+        } else {
+            it.onComplete()
+        }
+    }.subscribeOn(Schedulers.io())
+
+    fun insertPerson(person: Person): Completable = Completable.fromAction {
+        app.getDatabase()?.personDao()?.insert(person)
+    }.subscribeOn(Schedulers.io())
+
+    fun insertPeople(people: Array<Person>): Completable = Completable.fromAction {
+        app.getDatabase()?.personDao()?.insertAll(*people)
+    }.subscribeOn(Schedulers.io())
+}
+
+@Entity(primaryKeys = arrayOf("username"))
+data class Person(
+        val username: String,
+        val firstName: String,
+        val lastName: String)
+
+@Dao
+interface PersonDao {
+    companion object {
+        const val OBJ = "person"
+        const val USERNAME = "username"
+        const val FIRST_NAME = "first_name"
+        const val LAST_NAME = "last_name"
+    }
+
+    @Query("SELECT * FROM $OBJ")
+    fun getAll(): Flowable<List<Person>>
+
+    @Query("SELECT * FROM $OBJ WHERE $USERNAME = :username")
+    fun getPerson(username: String): Person
+
+    @Query("SELECT * FROM $OBJ WHERE $USERNAME IN (:usernames)")
+    fun getPeople(usernames: List<String>): Flowable<List<Person>>
+
+    @Insert
+    fun insert(person: Person)
+
+    @Insert
+    fun insertAll(vararg persons: Person)
+}
